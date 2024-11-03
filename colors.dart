adbc92aba2b46f7a11b0a4ad5bbb40b8785f5f8b
relmyer <jazklif@gmail.com>
2024-11-03 03:03:20 +0300

    Create colors.dart

---
+++ colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color color1 = Color(0xFF7D7B76);
+const Color color2 = Color(0xFFB0A89F);
+const Color color3 = Color(0xFF393536);
+const Color color4 = Color(0xFF4F4848);
+const Color color5 = Color(0xFF1D1B1B);
+const Color color6 = Color(0xFFE4DFD5);
